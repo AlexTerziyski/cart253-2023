55812abfaade48bbd34b4a48934024120d58cfb0
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-14 16:49:05 -0400

    A3: Completed the activity
    Apologies for not committing more often

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -1,11 +1,11 @@
 /**
- * Title of Project
- * Author Name
+ * Activity 3
+ * Alex Terziyski
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
  */
-
+ 
 "use strict";
 
 /**
@@ -15,12 +15,35 @@ function preload() {
 
 }
 
-
+let bg ={
+    r: 0,
+    g: 0,
+    b: 0
+};
+let circle1 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    growthRate: 1,
+    speed: 1,
+    fill: 255,
+    alpha: 200
+};
+let circle2 ={
+    x: 500,
+    y: 250,
+    size: 75,
+    sizeRatio: 0.5,
+    speed: -1,
+    fill: 255,
+    alpha: 200
+};
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500,500);
+    noStroke();
 }
 
 
@@ -28,5 +51,22 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    //Backgorund
+    background(bg.r,bg.g,bg.b);
+    bg.r = map(circle1.size,100,width,0,255);
 
+    //Left circle
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x,0,width/2);
+    circle1.size=circle1.size+ circle1.growthRate;
+    circle1.size=constrain(circle1.size,0,width);
+    fill(circle1.fill,circle1.alpha);
+    ellipse(circle1.x,circle1.y,circle1.size);
+
+    //Right circle
+    circle2.x = circle2.x + circle2.speed;
+    circle2.x = constrain(circle2.x,width/2,width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill,circle2.alpha);
+    ellipse(circle2.x,circle2.y,circle2.size);
 }
