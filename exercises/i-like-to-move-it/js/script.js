6246e7bffe7fd1b900127bb51e59d0e98849130b
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-14 16:57:57 -0400

    E1: Started exercise 1

---
+++ exercises/i-like-to-move-it/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Exercise: I like to move it! {
+ * Alexander Terziyski
+ * 
+ * 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
