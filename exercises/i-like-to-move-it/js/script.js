c4e5e81d4023fe2334bda7e07a93187f8d2d71b6
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-15 19:22:30 -0400

    Created the movements and attributes for the square and circle

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -14,13 +14,27 @@ function preload() {
 
 }
 
+//Declares the square object and its attributes
 let square1 = {
     x: 0,
     y: 250,
     square1Width: 100,
     square1Height: 100,
-    speed: 2
+    speed: 0.5,
+    fill: 0
 };
+
+//Declares the circle object and its attributes
+let circle1 = {
+    x: 250,
+    y: 0,
+    diameter: 100,
+    speed: 0.5,
+    fill: 0
+};
+
+
+
 
 
 /**
@@ -35,11 +49,34 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    //Randomizes the background color based on mouse position
     background(mouseX, mouseY, 0);
+    //The X and Y axis are now attributed to the center (rather than top left)
     rectMode(CENTER);
 
+    //Randomizes the square's width
+    square1.square1Width = random(50,150);
+    //Randomizes the square's height
+    square1.square1Height = random(50,150);
+    //Makes the square move to the right
     square1.x += square1.speed;
+    
+    //Fills the square's color
+    fill(square1.fill);
+    //Randomizes the square's color
+    square1.fill = random(0,255);
+    //Draws the square according to the previous specifications
     rect(square1.x, square1.y, square1.square1Width, square1.square1Height);
+
+    //Makes the circle travel down
+    circle1.y += circle1.speed;
+
+    //Uses map to adjust the circle's diamater according to the position of the mouse
+    circle1.diameter = map(mouseY,0,height,50,500);
+    //Fills the circle color
+    fill(circle1.fill);
+    //Draws the circle according to the previous specifications
+    circle(circle1.x, circle1.y, circle1.diameter);
     
 
 
