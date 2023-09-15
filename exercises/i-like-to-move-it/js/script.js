4db22ddfd01bd7e0d856d84a70cfcabeac0dab5e
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-15 16:27:14 -0400

    Created the Attributes for Square, Triangle, and Circle and Initialized them.

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Exercise: I like to move it! {
+ * Exercise: I like to move it!
  * Alexander Terziyski
  * 
  * 
@@ -14,12 +14,39 @@ function preload() {
 
 }
 
+//Attributes for the Square
+let squareX, squareY, squareSize, squareSpeed, squareColor;
+//Attributes for the Triangle
+let triangleX, triangleY, triangleSize, triangleSpeed, triangleColor;
+//Attributes for the Circle
+let circleX, circleY, circleSize, circleSpeed, circleColor;
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(500,500);
 
+    //Initializes the Square
+    squareX = random(width);
+    squareY = random(height);
+    squareSize = random(20, 50);
+    squareSpeed = random(1, 3);
+    squareColor = color(random(255), random(255), random(255));
+
+    //Initializes the Triangle
+    triangleX = random(width);
+    triangleY = random(height);
+    triangleSize = random(20, 50);
+    triangleSpeed = random(1, 3);
+    triangleColor = color(random(255), random(255), random(255));
+
+    //Initializes the Circle
+    circleX = random(width);
+    circleY = random(height);
+    circleSize = random(20, 50);
+    circleSpeed = random(1,3);
+    circleColor = color(random(255), random(255), random(255));
 }
 
 
