a9e7a5066c0b20aa876cba012e95b10be488779e
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-20 14:26:44 -0400

    Finished Conditionals introduction

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,26 +1,24 @@
 /**
- * Title of Project
- * Author Name
+ * Conditionals Experiments
+ * Alex Terziyski
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 5
 }
-
 
 /**
  * Description of setup
 */
 function setup() {
-
+createCanvas(500,500);
 }
 
 
@@ -28,5 +26,24 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+background(backgroundShade);
 
+circle.x = circle.x + circle.speed;
+
+if (circle.x > width){
+    circle.speed = -circle.speed;
+}
+if (circle.x < 0){
+    circle.speed = -circle.speed;
+}
+
+if (mouseY < height/2){
+    fill(255,0,0);
+}
+
+if (mouseY > height/2){
+    fill(0,255,0);
+}
+
+ellipse(circle.x,circle.y,circle.size);
 }
