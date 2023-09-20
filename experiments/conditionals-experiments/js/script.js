bb8e780ea4f8796244912e7c23fb3f8c6f40d858
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-20 15:14:26 -0400

    While and For Loops

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -7,7 +7,11 @@
 "use strict";
 
 
-let displayCircle = false;
+let caterpillar = {
+    x: 100,
+    y: 250,
+    segmentSize: 50
+}
 /**
  * Description of setup
 */
@@ -21,12 +25,26 @@ createCanvas(500,500);
 */
 function draw() {
     background(0);
+    noStroke();
+    fill(100,200,100);
 
-    if (mouseIsPressed){
-        displayCircle = true;
+    // let x = caterpillar.x;
+    // let numSegments = 10;
+    // let segmentsDrawn = 0;
+
+    // while (segmentsDrawn < numSegments) {
+    //     ellipse(x,caterpillar.y,caterpillar.segmentSize);
+    //     x = x +40;
+    //     segmentsDrawn++;
+    // }
+    let x = caterpillar.x;
+    let numSegments = 10;
+
+    for (let i = 0; segmentsDrawn < numSegments;i++ ){
+        ellipse(x,caterpillar.y,caterpillar.segmentSize);
+        x=x+40;
     }
 
-    if (displayCircle) {
-    ellipse(250,250,100,100);
-}    
-}
+}   
+
+
