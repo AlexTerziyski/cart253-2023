754d97a376611601920a902f52a5424d3622b4dd
AlexTerziyski <alexander.terziyski@mail.concordia.ca>
2023-09-25 22:45:55 -0400

    E2: Made it so that the user must drag

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Exercise 2: Dodge Em
+ * Alexander Terziyski
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -15,12 +15,38 @@ function preload() {
 
 }
 
+let covid19 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0
+    }
+};
+
+let user = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255
+};
+
+let numStatic = 1500;
+
 
 /**
  * Description of setup
 */
 function setup() {
+    createCanvas(windowWidth,windowHeight);
 
+    covid19.y = random(0,height);
+    covid19.vx = covid19.speed;
 }
 
 
@@ -28,5 +54,48 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+
+    // Displays static
+    for (let i = 0; i < numStatic; i++) {
+        let x = random(0,width);
+        let y = random(0,height);
+        stroke(255);
+        point(x,y);
+    }
+    
+    //Covid movement
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    if (covid19.x > width){
+        covid19.x = 0;
+        covid19.y = random(0,height);
+    }
+    //Display Covid
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x,covid19.y,covid19.size);
+
+
+    //Display user
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+
+    //Check for catching Covid
+    let d = dist(user.x, user.y, covid19.x, covid19.y);
+    if (d < covid19.size/2 + user.size/2){
+        noLoop();
+    }
 
 }
+//User movement
+function mouseDragged() {
+    if (mouseIsPressed) {
+        user.x = mouseX;
+        user.y = mouseY;
+    }
+}
+    
+   
+
+    
